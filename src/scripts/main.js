4dc482f61d145144460395cc96d6f1a426470136
Hanna Yatskiv <hanna.ada7@gmail.com>
2025-11-05 10:21:54 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,29 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  const wall = e.target.closest('.wall');
+
+  if (!wall) {
+    return;
+  }
+
+  const wallCoords = wall.getBoundingClientRect();
+  // Розрахунок нових координат відносно "стіни"//
+  let newTop = Math.max(
+    e.clientY - wallCoords.top - spider.clientHeight / 2,
+    0,
+  );
+
+  let newLeft = Math.max(
+    e.clientX - wallCoords.left - spider.clientWidth / 2,
+    0,
+  );
+
+  // Обмеження руху павука в межах стіни//
+  newTop = Math.min(newTop, wall.clientHeight - spider.clientHeight);
+  newLeft = Math.min(newLeft, wall.clientWidth - spider.clientWidth);
+  // Переміщення павука//
+  spider.style.top = newTop + 'px';
+  spider.style.left = newLeft + 'px';
 });
